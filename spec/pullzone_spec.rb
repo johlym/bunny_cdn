1612f81b3f3437d1fd223e4a84a94e4d755a6796
Johnathan Lyman <jelyman@gmail.com>
2020-07-03 14:46:12 -0700

    Make tests work again

--- spec/pullzone_spec.rb
+++ spec/pullzone_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 RSpec.describe BunnyCdn::Pullzone do
   before(:each) do
     BunnyCdn.configure do |config|
-      config.api_key = ENV['API_KEY']
+      config.api_key = ENV["BCDN_API_KEY"]
     end
     headers = 
     {
@@ -16,21 +16,24 @@ RSpec.describe BunnyCdn::Pullzone do
   describe "#getAllPullzones" do
     before do
       stub_request(:get, "https://bunnycdn.com/api/pullzone").
-        with(
-          headers: {
-            :content_type => 'application/json',
-            :accept => 'application/json',
-            :access_key => BunnyCdn.configuration.api_key
-          }).to_return(status: 200)
+         with(
+           headers: {
+            'Accept' => 'application/json',
+            'Accesskey' =>  BunnyCdn.configuration.api_key,
+            'Content-Type' => 'application/json'
+           }
+          ).
+         to_return(status: 200, body: "", headers: {})
     end
     it "gets all pullzones" do
       BunnyCdn::Pullzone.getAllPullzones
       expect(WebMock).to have_requested(:get, "https://bunnycdn.com/api/pullzone").
         with(headers: {
-        :content_type => 'application/json', 
-        :accept => 'application/json',
-        :access_key => BunnyCdn.configuration.api_key
-        }).once
+          'Accept' => 'application/json',
+          'Accesskey' =>  BunnyCdn.configuration.api_key,
+          'Content-Type' => 'application/json'
+        }
+      ).once
     end
   end
 
@@ -39,30 +42,32 @@ RSpec.describe BunnyCdn::Pullzone do
       stub_request(:post, "https://bunnycdn.com/api/pullzone").
         with(
           headers: {
-            :content_type => 'application/json',
-            :accept => 'application/json',
-            :access_key => BunnyCdn.configuration.api_key
+            'Accept' => 'application/json',
+            'Accesskey' =>  BunnyCdn.configuration.api_key,
+            'Content-Type' => 'application/json'
           },
           body: {
             :name => 'test',
             :type => 0,
             :originUrl => 'http://example.com'
-          }).to_return(status: 200)
+          }
+        ).to_return(status: 200)
     end
     it "gets all pullzones" do
       BunnyCdn::Pullzone.createPullzone('test', 0, 'http://example.com')
       expect(WebMock).to have_requested(:post, "https://bunnycdn.com/api/pullzone").
       with(
         headers: {
-          :content_type => 'application/json',
-          :accept => 'application/json',
-          :access_key => BunnyCdn.configuration.api_key
+          'Accept' => 'application/json',
+          'Accesskey' =>  BunnyCdn.configuration.api_key,
+          'Content-Type' => 'application/json'
         },
         body: {
           :name => 'test',
           :type => 0,
           :originUrl => 'http://example.com'
-        }).once
+        }
+      ).once
     end
   end
 
@@ -71,51 +76,59 @@ RSpec.describe BunnyCdn::Pullzone do
       stub_request(:get, "https://bunnycdn.com/api/pullzone/1234").
         with(
           headers: {
-            :content_type => 'application/json',
-            :accept => 'application/json',
-            :access_key => BunnyCdn.configuration.api_key
-          }).to_return(status: 200)
+            'Accept' => 'application/json',
+            'Accesskey' =>  BunnyCdn.configuration.api_key,
+            'Content-Type' => 'application/json'
+          }
+        ).to_return(status: 200)
     end
     it "gets a single pullzone by ID" do
       BunnyCdn::Pullzone.getSinglePullzone(1234)
       expect(WebMock).to have_requested(:get, "https://bunnycdn.com/api/pullzone/1234").
       with(
         headers: {
-          :content_type => 'application/json',
-          :accept => 'application/json',
-          :access_key => BunnyCdn.configuration.api_key
-        }).once
+          'Accept' => 'application/json',
+          'Accesskey' =>  BunnyCdn.configuration.api_key,
+          'Content-Type' => 'application/json'
+        }
+      ).once
     end
   end
 
   describe "#deletePullzone" do
     before do
       stub_request(:delete, "https://bunnycdn.com/api/pullzone/1234").
-      with(headers: {
-        :content_type => 'application/json',
-        :accept => 'application/json',
-        :access_key => BunnyCdn.configuration.api_key
-      }).to_return(status: 200)
+      with(
+        headers: {
+          'Accept' => 'application/json',
+          'Accesskey' =>  BunnyCdn.configuration.api_key,
+          'Content-Type' => 'application/json'
+        }
+      ).to_return(status: 200)
     end
     it "deletes pullzone by ID" do
       BunnyCdn::Pullzone.deletePullzone(1234)
       expect(WebMock).to have_requested(:delete, "https://bunnycdn.com/api/pullzone/1234").
-      with(headers: {
-        :content_type => 'application/json',
-        :accept => 'application/json',
-        :access_key => BunnyCdn.configuration.api_key
-      }).once
+      with(
+        headers: {
+          'Accept' => 'application/json',
+          'Accesskey' =>  BunnyCdn.configuration.api_key,
+          'Content-Type' => 'application/json'
+        }
+      ).once
     end
   end
 
   describe "#purgeCache" do
     before do
       stub_request(:post, "https://bunnycdn.com/api/pullzone/1234/purgeCache").
-        with(headers: {
-          :content_type => 'application/json',
-          :accept => 'application/json',
-          :access_key => BunnyCdn.configuration.api_key
-        }).to_return(status: 200)
+        with(
+          headers: {
+            'Accept' => 'application/json',
+            'Accesskey' =>  BunnyCdn.configuration.api_key,
+            'Content-Type' => 'application/json'
+          }
+        ).to_return(status: 200)
     end
     it "purges cache for the specified pullzone" do
       BunnyCdn::Pullzone.purgeCache(1234)
--- spec/storage_spec.rb
+++ spec/storage_spec.rb
@@ -3,11 +3,12 @@ require 'spec_helper'
 RSpec.describe BunnyCdn::Storage do
   before(:each) do
     BunnyCdn.configure do |config|
-      config.storage_zone = ENV['STORAGE_ZONE']
-      config.access_key = ENV['ACCESS_KEY']
+      config.api_key = ENV["BCDN_API_KEY"]
+      config.storage_zone = ENV["BCDN_STORAGE_ZONE"]
+      config.access_key = ENV["BCDN_ACCESS_KEY"]
     end
     headers = {
-      :access_key => BunnyCdn.configuration.access_key
+      :AccessKey => BunnyCdn.configuration.access_key
     }
   end
 
@@ -16,19 +17,44 @@ RSpec.describe BunnyCdn::Storage do
       stub_request(:get, "https://storage.bunnycdn.com/#{BunnyCdn.configuration.storage_zone}/#{ENV['FILE_PATH']}").
         with(
           headers: {
-            :access_key => BunnyCdn.configuration.access_key
+            :AccessKey => BunnyCdn.configuration.access_key
+          }).
+        to_return(status: 200, body: "")
+
+      stub_request(:get, "https://ny.storage.bunnycdn.com/#{BunnyCdn.configuration.storage_zone}/#{ENV['FILE_PATH']}").
+        with(
+          headers: {
+            :AccessKey => BunnyCdn.configuration.access_key
           }).
         to_return(status: 200, body: "")
     end
     it "gets all files from storage zone" do
+      BunnyCdn.configure do |config|
+        config.region = "eu"
+      end
       headers = {
-        :access_key => BunnyCdn.configuration.access_key
+        :AccessKey => BunnyCdn.configuration.access_key
       }
       BunnyCdn::Storage.getZoneFiles
       # RestClient.get("https://storage.bunnycdn.com/#{BunnyCdn.configuration.storage_zone}/", headers)
       expect(WebMock).to have_requested(:get ,"https://storage.bunnycdn.com/#{BunnyCdn.configuration.storage_zone}/").
         with(headers: {
-          :access_key => BunnyCdn.configuration.access_key
+          :AccessKey => BunnyCdn.configuration.access_key
+        }).once
+    end
+
+    it "gets all files from non-EU storage zone" do
+      BunnyCdn.configure do |config|
+        config.region = "ny"
+      end
+      headers = {
+        :AccessKey => BunnyCdn.configuration.access_key
+      }
+      BunnyCdn::Storage.getZoneFiles
+      # RestClient.get("https://storage.bunnycdn.com/#{BunnyCdn.configuration.storage_zone}/", headers)
+      expect(WebMock).to have_requested(:get ,"https://ny.storage.bunnycdn.com/#{BunnyCdn.configuration.storage_zone}/").
+        with(headers: {
+          :AccessKey => BunnyCdn.configuration.access_key
         }).once
     end
   end
@@ -37,22 +63,50 @@ RSpec.describe BunnyCdn::Storage do
     before do
       file = 'test_file.txt'
       stub_request(:get, "https://storage.bunnycdn.com/#{BunnyCdn.configuration.storage_zone}/#{ENV['FILE_PATH']}/#{file}").
-        with(
-          headers: {
-            :access_key => BunnyCdn.configuration.access_key
-          }).
-        to_return(status: 200)
+      with(
+        headers: {
+          :AccessKey => BunnyCdn.configuration.access_key
+        }
+      ).
+      to_return(status: 200)
+
+      stub_request(:get, "https://ny.storage.bunnycdn.com/#{BunnyCdn.configuration.storage_zone}/#{ENV['FILE_PATH']}/#{file}").
+      with(
+        headers: {
+          :AccessKey => BunnyCdn.configuration.access_key
+        }
+      ).
+      to_return(status: 200)
     end
     it "gets a single file from the storage zone" do
+      BunnyCdn.configure do |config|
+        config.region = "eu"
+      end
       headers = {
-        :access_key => BunnyCdn.configuration.access_key
+        :AccessKey => BunnyCdn.configuration.access_key
       }
       path = ENV['FILE_PATH']
       file = 'test_file.txt'
       BunnyCdn::Storage.getFile(path, file)
       expect(WebMock).to have_requested(:get ,"https://storage.bunnycdn.com/#{BunnyCdn.configuration.storage_zone}/#{path}/#{file}").
         with(headers: {
-          :access_key => BunnyCdn.configuration.access_key
+          :AccessKey => BunnyCdn.configuration.access_key
+        }).once
+    end
+
+    it "gets a single file from the non-EU storage zone" do
+      BunnyCdn.configure do |config|
+        config.region = "ny"
+      end
+      headers = {
+        :AccessKey => BunnyCdn.configuration.access_key
+      }
+      path = ENV['FILE_PATH']
+      file = 'test_file.txt'
+      BunnyCdn::Storage.getFile(path, file)
+      expect(WebMock).to have_requested(:get ,"https://ny.storage.bunnycdn.com/#{BunnyCdn.configuration.storage_zone}/#{path}/#{file}").
+        with(headers: {
+          :AccessKey => BunnyCdn.configuration.access_key
         }).once
     end
   end
@@ -61,15 +115,29 @@ RSpec.describe BunnyCdn::Storage do
     before do
       file = File.join('spec', 'test_file.txt')
       stub_request(:put, "https://storage.bunnycdn.com/#{BunnyCdn.configuration.storage_zone}/#{ENV['FILE_PATH']}/#{File.basename(file)}").
-        with(
-          headers: {
-            :access_key => BunnyCdn.configuration.access_key,
-            :checksum => ''
-          },
-          body: File.read(file)).
-        to_return(status: 200)
+      with(
+        headers: {
+          :access_key => BunnyCdn.configuration.access_key,
+          :checksum => ''
+        },
+        body: File.read(file)
+      ).
+      to_return(status: 200)
+
+      stub_request(:put, "https://ny.storage.bunnycdn.com/#{BunnyCdn.configuration.storage_zone}/#{ENV['FILE_PATH']}/#{File.basename(file)}").
+      with(
+        headers: {
+          :access_key => BunnyCdn.configuration.access_key,
+          :checksum => ''
+        },
+        body: File.read(file)
+      ).
+      to_return(status: 200)
     end
     it "uploads file to storage zone" do
+      BunnyCdn.configure do |config|
+        config.region = "eu"
+      end
       headers = {
         :access_key => BunnyCdn.configuration.access_key,
         :checksum => ''
@@ -79,6 +147,25 @@ RSpec.describe BunnyCdn::Storage do
       # BunnyCdn::Storage.uploadFile(path, file)
       RestClient.put("https://storage.bunnycdn.com/#{BunnyCdn.configuration.storage_zone}/#{ENV['FILE_PATH']}/#{File.basename(file)}", File.read(file), headers)
       expect(WebMock).to have_requested(:put ,"https://storage.bunnycdn.com/#{BunnyCdn.configuration.storage_zone}/#{ENV['FILE_PATH']}/#{File.basename(file)}").
+        with(headers: {
+          :access_key => BunnyCdn.configuration.access_key,
+          :checksum => ''
+        }).once
+    end
+
+    it "uploads file to storage zone" do
+      BunnyCdn.configure do |config|
+        config.region = "ny"
+      end
+      headers = {
+        :access_key => BunnyCdn.configuration.access_key,
+        :checksum => ''
+      }
+      path = ENV['FILE_PATH']
+      file = File.join('spec', 'test_file.txt')
+      # BunnyCdn::Storage.uploadFile(path, file)
+      RestClient.put("https://ny.storage.bunnycdn.com/#{BunnyCdn.configuration.storage_zone}/#{ENV['FILE_PATH']}/#{File.basename(file)}", File.read(file), headers)
+      expect(WebMock).to have_requested(:put ,"https://ny.storage.bunnycdn.com/#{BunnyCdn.configuration.storage_zone}/#{ENV['FILE_PATH']}/#{File.basename(file)}").
         with(headers: {
           :access_key => BunnyCdn.configuration.access_key,
           :checksum => ''
